9fc4ce8d4cc22d58bf210ad07fec9c92549e783a
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-07-24 12:10:27 -0400

    trying to create an image with slanted base

--- front-page.php
+++ front-page.php
@@ -1,5 +1,20 @@
 <?php get_header(); ?>
 
+<!-- BANNER  -->
+<div class="hero">
+  <figure>
+    <img src="https://unsplash.it/1600/900?image=830" alt="" />
+    <figcaption>
+      <h1>Angled Edge</h1>
+      <p>Image with CSS clip-path</p>
+    </figcaption>
+  </figure>
+</div>
+
+<div class="promo">
+  <h1>Angled Edge</h1>
+  <p>CSS background with CSS clip-path</p>
+</div>
 
 
 <?php get_footer(); ?>
