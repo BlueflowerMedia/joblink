a312753e1c11970916bce7b2bbdc61312bb83f7a
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-08-09 17:43:35 -0400

    footer fixed

--- footer.php
+++ footer.php
@@ -13,19 +13,18 @@
 
 	</div><!-- #content -->
 
-	<div class="margin-for-footer"></div>
 	<footer id="colophon" class="site-footer">
-		<div class="container flex-row">
-			<div class="footer-left flex1">
+		<div class="container">
+			<div class="footer-left">
 				<h3>Have any questions?</h3>
 				<?php echo do_shortcode('[contact-form-7 id="12" title="Contact form footer"]') ?>
 	    </div>
-	    <div class="footer-right flex1">
+	    <div class="footer-right">
 				<h3>Contact Info.</h3>
 				<p><strong>Call: </strong>01871775547</p><br>
 				<p><strong>Email: </strong>infoatnorthamerica@gmail.com</p><br>
 				<p><strong>Address:</strong></p><br>
-				<p>House #10, Road #06, Block - B,</p>
+				<p>House #10, Road #06, Block-B,</p>
 				<p> Banasree, Rampura,</p>
 				<p>Dhaka - 1219, Bangladesh</p><br>
 				<p><strong>Payment Info:</strong></p><br>
